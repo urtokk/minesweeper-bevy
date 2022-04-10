f44ff2f33e70df9996ba42069b0f5b66e1426188
Torben Diegel <xendios@gmail.com>
2022-04-10 19:28:51 +0200

    basic main

--- src/main.rs
+++ src/main.rs
@@ -1,3 +1,24 @@
+use bevy::prelude::*;
+
+#[cfg(feature = "debug")]
+use bevy_inspector_egui::WorldInspectorPlugin;
+
 fn main() {
-    println!("Hello, world!");
+    let mut app = App::new();
+
+    app.insert_resource(WindowDescriptor {
+        title: "Mine Sweeper!".to_string(),
+        width: 700.,
+        height: 800.,
+        ..Default::default()
+    })
+        .add_plugins(DefaultPlugins);
+    #[cfg(feature = "debug")]
+    app.add_plugin(WorldInspectorPlugin::new());
+    app.add_startup_system(camera_setup);
+    app.run();
+}
+
+fn camera_setup(mut commands: Commands) {
+    commands.spawn_bundle(OrthographicCameraBundle::new_2d());
 }
